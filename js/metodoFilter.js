aac852da5413f244b1be653c141c5234cedf5207
renata-aline <renatalinec@gmail.com>
2023-07-19 18:36:08 -0300

    valor total com metodo reduce

--- js/metodoFilter.js
+++ js/metodoFilter.js
@@ -8,7 +8,8 @@ function filtrarLivros(){
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirLivrosNaTela(livrosFiltrados);
     if (categoria == 'disponivel'){
-        exibirValorTotalDeLivrosNaTela();
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDeLivrosNaTela(valorTotal);
     }
 }
 
@@ -20,10 +21,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0);
 }
 
-function exibirValorTotalDeLivrosNaTela(){
+function exibirValorTotalDeLivrosNaTela(valorTotal){
     elementoComValorTotalDeLivrosDisponivel.innerHTML = `
     <div class="livros__disponiveis">
-      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>
 
     `
